57156270f80f54bc383ccbf5d64177ab5b545358
MFCardozo <manu.mclfy6@gmail.com>
2025-03-29 23:28:55 -0300

    rabbitMQ implementation

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,4 +1,6 @@
 using AuctionService.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,21 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 });
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x  =>
+{
+        x.AddEntityFrameworkOutbox<AuctionDbContext>( o =>
+        {
+            o.QueryDelay = TimeSpan.FromSeconds(10);
+            
+            o.UsePostgres();
+            o.UseBusOutbox();
+        }); 
+
+        x.UsingRabbitMq((context, cfg) =>
+        {
+            cfg.ConfigureEndpoints(context);
+        });
+});
 
 var app = builder.Build();
 
---
+++ src/SearchService/requestHelpers/MappingProfiles.cs
@@ -0,0 +1,15 @@
+using System;
+using AutoMapper;
+using Contracts;
+using SearchService.Models;
+
+namespace SearchService.requestHelpers;
+
+public class MappingProfiles : Profile
+{
+
+public MappingProfiles()
+{
+    CreateMap<AuctionCreated, Item>();
+}
+}
